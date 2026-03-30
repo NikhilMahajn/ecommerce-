e989c27c9dee68c867be88c7359e5a259db4cc82
NikhilMahajn <mahajannikhil0016@gmail.com>
2026-03-30 21:46:57 +0530

    backend init

---
+++ Ecommerce-Api/app/main.py
@@ -0,0 +1,65 @@
+from app.routers import products, categories, carts, users, auth, accounts
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+description = """
+Welcome to the E-commerce API! 🚀
+
+This API provides a comprehensive set of functionalities for managing your e-commerce platform.
+
+Key features include:
+
+- **Crud**
+	- Create, Read, Update, and Delete endpoints.
+- **Search**
+	- Find specific information with parameters and pagination.
+- **Auth**
+	- Verify user/system identity.
+	- Secure with Access and Refresh tokens.
+- **Permission**
+	- Assign roles with specific permissions.
+	- Different access levels for User/Admin.
+- **Validation**
+	- Ensure accurate and secure input data.
+
+
+For any inquiries, please contact:
+
+* Github: https://github.com/aliseyedi01
+"""
+
+
+app = FastAPI(
+    description=description,
+    title="E-commerce API",
+    version="1.0.0",
+    swagger_ui_parameters={
+        "syntaxHighlight.theme": "monokai",
+        "layout": "BaseLayout",
+        "filter": True,
+        "tryItOutEnabled": True,
+        "onComplete": "Ok"
+    },
+)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(products.router)
+app.include_router(categories.router)
+app.include_router(carts.router)
+app.include_router(users.router)
+app.include_router(accounts.router)
+app.include_router(auth.router)
