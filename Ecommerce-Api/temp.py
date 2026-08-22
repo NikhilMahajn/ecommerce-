bcdc473d025946249a76bbdad864c59e67a4f8a2
NikhilMahajn <mahajannikhil0016@gmail.com>
2026-08-22 17:45:11 +0530

    basic agent functionality implemented

--- Ecommerce-Api/temp.py
+++ Ecommerce-Api/temp.py
@@ -4,7 +4,7 @@ import os
 url = "https://api.groq.com/openai/v1/models"
 
 headers = {
-    "Authorization": f"Bearer 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",
+    "Authorization": f"Bearer ",
     "Content-Type": "application/json"
 }
 
